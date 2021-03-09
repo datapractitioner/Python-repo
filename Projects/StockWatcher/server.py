339bc19121dcbfc11a3e0a88dccddf327896fc44
datapractitioner <teso.tiziano@outlook.it>
2021-03-09 21:49:32 +0100

    Rename
    Rename

---
+++ Projects/StockWatcher/server.py
@@ -0,0 +1,13 @@
+from typing import Optional
+from fastapi import FastAPI
+import pandas_datareader as pdr
+
+'''
+    The Backend will be supported with FastAPI
+'''
+
+#app = FastAPI()
+
+#@app.get('/')
+#def read_root():
+#    return {'Hello' : 'World'}
